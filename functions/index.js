1bc974da08bb0a53f11f5f3f6682912ae8f92f61
vivekmalumel <vivek.kply@gmail.com>
2019-07-13 15:39:23 +0530

    Express Added

--- functions/index.js
+++ functions/index.js
@@ -1,37 +1,59 @@
 const functions = require('firebase-functions');
 const admin=require('firebase-admin')
-
+const firebase=require('firebase')
 
 admin.initializeApp();
+
+const express=require('express');
+const app=express();
+
+const config = {
+    apiKey: "AIzaSyBnsT7dSkYVCzOQofTE7EJDbM3zqZ1i0kU",
+    authDomain: "social-api-3bbea.firebaseapp.com",
+    databaseURL: "https://social-api-3bbea.firebaseio.com",
+    projectId: "social-api-3bbea",
+    storageBucket: "social-api-3bbea.appspot.com",
+    messagingSenderId: "600533108533",
+    appId: "1:600533108533:web:a60a209ac3df07b2"
+  };
+
+firebase.initializeApp(config);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
 
-exports.getSreams=functions.https.onRequest((request,response)=>{
+app.get('/screams',(req,res)=>{
     let screams=[];
-   admin.firestore().collection('scream').get()
+   admin.firestore().collection('scream').orderBy('createdAt', 'desc').get()
     .then(data=>{
             data.forEach(doc=>{
-                screams.push(doc.data());
+                screams.push({
+                    screamId:doc.id,
+                    body:doc.data().body,
+                    userHandle:doc.data().userHandle,
+                    createdAt:doc.data().createdAt
+                });
             })
-           return response.json(screams);
+           return res.json(screams);
     })
     .catch(err=>{
+        res.status(500).json({error:"something went wrong"});
         console.error(err);
     })
 })
 
-exports.createScream=functions.https.onRequest((req,res)=>{
-    if(req.method!=='POST')
-        return res.status(400).json({error:"Method Not Allowed"});
+app.post('/scream',(req,res)=>{
+    // if(req.method!=='POST')
+    //     return res.status(400).json({error:"Method Not Allowed"});
     let newScream={
         body:req.body.body,
         userHandle:req.body.userHandle,
-        cretaedAt:admin.firestore.Timestamp.fromDate(new Date())
+        createdAt: new Date().toISOString()
+        // createdAt:admin.firestore.Timestamp.fromDate(new Date())
     }
     admin.firestore().collection('scream').add(newScream)
     .then(doc=>{
@@ -42,3 +64,7 @@ exports.createScream=functions.https.onRequest((req,res)=>{
         console.log(err);
     })
 })
+
+//https://baseurl.api
+
+exports.api=functions.https.onRequest(app);
